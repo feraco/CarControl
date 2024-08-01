90a7416298a70b17790771ff59a45f27fb457f1a
Fred Feraco <frederick.feraco@gmail.com>
2024-08-01 14:40:39 -0400

    Update almostsumo.ino

--- examples/almostsumo/almostsumo.ino
+++ examples/almostsumo/almostsumo.ino
@@ -1,10 +1,8 @@
-
 #include <CarControl.h>
 #include <Ultrasonic.h>
 #include <FastLED.h>
 #include <Servo.h>
 #include "MPU6050.h"
-#include <Ultrasonic.h>
 
 // Define pin numbers for motors and other components
 
@@ -38,35 +36,19 @@ void loop() {
   int lineThreshold = 120; // Adjusted based on your sensor readings
 
   // Check for the opponent
-  int distanceToOpponent = car.getDistanceToObstacle();
-  if (distanceToOpponent < 15) {
-    car.lightRGBForDuration(CRGB::Red, 500); // Red for aggressive forward movement
-    car.moveForward(255, 50); // Move forward at full speed
-  } else {
-    // Determine the robot's movement based on the sensor readings
+
     if (middleSensor < lineThreshold) {
       // Middle sensor detects white surface - move forward
      // car.lightRGBForDuration(CRGB::Green, 500);
-      car.moveForward(200, 50);
-    } else {
+      car.moveForward(255, 30);
+   
       // Middle sensor detects black line - adjust direction
-      if (leftSensor > lineThreshold) {
-        // Left sensor detects white - turn left
-        car.lightRGBForDuration(CRGB::Yellow, 500);
-        car.turnLeft(200, 400);
-      } else if (rightSensor > lineThreshold) {
-        // Right sensor detects white - turn right
-        car.lightRGBForDuration(CRGB::Blue, 500);
-        car.turnRight(200, 400);
+     
       } else {
         // All sensors detect black - back up and turn around
-        car.lightRGBForDuration(CRGB::Purple, 500);
-        car.moveBackward(200, 100);
-        car.turnAround();
+       // car.lightRGBForDuration(CRGB::Purple, 500);
+        car.moveBackward(200, 200);
+                car.turnRight(200, 400);
+
       }
     }
-  }
-
-  // Reset LED color to black after each action
-  car.lightRGBForDuration(CRGB::Black, 500);
-}
